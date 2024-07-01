6a819b4b9b1c88ecd5af005c43b5e18b7d9a9e85
PutumaS <161317482+PutumaS@users.noreply.github.com>
2024-07-01 09:18:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const downloadBtn = document.querySelector('a');
+
+
+downloadBtn.addEventListener('click', ()=>{
+
+    alert('CV Downloaded!');
+});
+
+const main = document.querySelector('main');
+main.classList.add('fade-in');
